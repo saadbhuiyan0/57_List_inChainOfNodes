deced2ff3ca7e0d4f85c95b05f554c1a8f238504
Saad Bhuiyan <ssaad00@stuy.edu>
2019-03-25 23:02:01 -0400

    added methods (needs lot of work)

---
+++ List_inChainOfNodes.java
@@ -0,0 +1,107 @@
+/**
+  Represent a list, implemented in a chain of nodes
+ */
+
+public class List_inChainOfNodes{
+    private Node headReference;
+
+    /**
+      Construct an empty list
+      The default constructor is fine:
+      take zero arguments
+      do nothing
+     */
+
+    /**
+      @return the number of elements in this list
+     */
+    public int size() {
+        // recursive approach seems more perspicuous
+        if( headReference == null) return 0;
+        else return size( headReference);
+    }
+
+    // recursively-called helper
+    private int size( Node startingAt) {
+        Node next = startingAt.getReferenceToNextNode();
+        if( next == null) return 1;
+        else return 1+ size( next);
+    }
+
+
+     /**
+       @return a string representation of this list,
+       format:
+           # elements [element0,element1,element2,]
+      */
+    public String toString() {
+        String stringRep = size() + " elements [";
+
+        for( Node node = headReference
+           ; node != null
+           ; node = node.getReferenceToNextNode() )
+            stringRep += node.getCargoReference() + ",";
+        return stringRep + "]";
+    }
+
+    /**
+      Append @value to the head of this list.
+
+      @return true, in keeping with conventions yet to be discussed
+     */
+    public boolean addAsHead( Object val) {
+        headReference = new Node( val, headReference);
+        return true;
+    }
+
+    public boolean set( int index, Object cargo) {
+        Node targetNode = headReference;
+        int iterate = index;
+        while ( iterate > 0) {
+            targetNode = targetNode.getReferenceToNextNode();
+            iterate--;
+        }
+        targetNode = new Node( cargo, targetNode.getReferenceToNextNode());
+        return true;
+    }
+
+    public Object get( int index) {
+        Node cargoNode = headReference;
+        int iterate = index;
+        while ( iterate > 0) {
+            cargoNode = cargoNode.getReferenceToNextNode();
+            iterate--;
+        }
+        return cargoNode.getCargoReference();
+    }
+
+    public boolean add( int index, Object cargo) {
+        Node newNode = new Node( cargo);
+        Node nextNode = headReference;
+        Node previousNode = headReference;
+        int iterate = index;
+        while ( iterate > 0) {
+            previousNode = nextNode;
+            nextNode = nextNode.getReferenceToNextNode();
+            iterate--;
+        }
+        newNode.setReferenceToNextNode( nextNode);
+        previousNode.setReferenceToNextNode( newNode);
+        return true;
+    }
+
+    public boolean remove( int index) {
+        Node nextNode = headReference;
+        Node previousNode = headReference;
+        int iterate = index;
+        while ( iterate > 0) {
+            previousNode = nextNode;
+            nextNode = nextNode.getReferenceToNextNode();
+            iterate--;
+        }
+        previousNode.setReferenceToNextNode( nextNode);
+        return true;
+    }
+
+
+}
---
+++ UserOfList.java
@@ -0,0 +1,33 @@
+/**
+ Test list features.
+ */
+public class UserOfList {
+    public static void main( String[] args ) {
+        List_inChainOfNodes list = new List_inChainOfNodes();
+
+        System.out.println( "number of elements: " + list.size() );
+        System.out.println( "empty list: " + list
+                          + System.lineSeparator());
+
+        /* Populate the list with elements.
+           Create the test data in an array, for the programming
+           convenience of being able to loop through it.
+        */
+        String[] elements = new String[]{"y","u","t","S",};
+            /* convenient syntax      ^^^^^^^^^^^^^^^^^^^^
+               for convenient way to init an array of Strings */
+        int elemIndex;
+        for( String elem : elements ) {
+            list.addAsHead( elem);
+            System.out.println( "number of elements: " + list.size() );
+        }
+        System.out.println( "populated list: " + list
+                          + System.lineSeparator());
+        
+        System.out.println( list.set( 0, "s"));
+        System.out.println( "set S - s: " + list
+                          + System.lineSeparator());
+
+
+    }
+}
